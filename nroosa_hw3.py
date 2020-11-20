9ef3d26c60fa6be5048e54bc397dd38e5640feb4
nolanroosa <ndr34@pitt.edu>
2020-11-20 16:51:03 -0500

    Update nroosa_hw3.py

--- nroosa_hw3.py
+++ nroosa_hw3.py
@@ -19,5 +19,12 @@ query = 'SELECT name FROM sqlite_master WHERE type="table"'
 cursor.execute(query)
 row = cursor.fetchall()
 
-query = 'PRAGMA table_info (EMPLOYEES)'
-cursor.execute(query)
+for name in row:
+    print('\nTable: {}'.format(name[0]))
+    queryY = 'PRAGMA table_info('+name[0]+')'
+    cursor.execute(queryY)
+    colNames = cursor.fetchall()
+    print('Columns: ', end = '')
+    for i in colNames:
+        print(i[1], '| ', end = '')
+    print('')
